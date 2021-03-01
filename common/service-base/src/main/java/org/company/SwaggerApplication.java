3ffb0421b4c8470bd2221137bf3fe87c394c2757
yaochunguang <yaochunguang@ava.com.cn>
2021-03-01 19:46:32 +0800

    配置swagger

--- common/service-base/src/main/java/org/company/SwaggerApplication.java
+++ common/service-base/src/main/java/org/company/SwaggerApplication.java
@@ -1,19 +1,17 @@
-package com.company;
+package org.company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author: yaochunguang
- * @date: 2021-02-27 16:05
+ * @date: 2021-03-01 19:27
  * @description: 启动类
  **/
-@ComponentScan(basePackages = {"com.company"})
 @SpringBootApplication
-public class EduApplication {
+public class SwaggerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EduApplication.class, args);
+        SpringApplication.run(SwaggerApplication.class, args);
     }
 }
