4414f57e2fe76743ba9750b242924524dd6ce0b8
yaochunguang <yaochunguang@ava.com.cn>
2021-03-03 19:56:52 +0800

    注释掉springboot打包插件；定义统一返回结果

---
+++ common/common-utils/src/main/java/com/company/common/Result.java
@@ -0,0 +1,83 @@
+package com.company.common;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: yaochunguang
+ * @date: 2021-03-03 19:47
+ * @description: 统一返回结果
+ **/
+@Data
+public class Result {
+
+    @ApiModelProperty(value = "是否成功")
+    private Boolean success;
+
+    @ApiModelProperty(value = "返回码")
+    private Integer code;
+
+    @ApiModelProperty(value = "返回消息")
+    private String message;
+
+    @ApiModelProperty(value = "返回数据")
+    private Map<String, Object> data = new HashMap<>();
+
+    /**
+     * 私有化构造函数
+     */
+    private Result() {
+    }
+
+    /**
+     * 成功
+     *
+     * @return
+     */
+    public static Result ok() {
+        Result result = new Result();
+        result.setSuccess(true);
+        result.setCode(ResultCode.SUCCESS);
+        result.setMessage("成功");
+        return result;
+    }
+
+    /**
+     * 失败
+     *
+     * @return
+     */
+    public static Result error() {
+        Result result = new Result();
+        result.setSuccess(false);
+        result.setCode(ResultCode.ERROR);
+        result.setMessage("失败");
+        return result;
+    }
+
+    /************************ 下面的方法都返回this，可以实现链式变成调用 eg: Result.ok().data("items", list); *****************************/
+
+
+    public Result success(Boolean success) {
+        this.setSuccess(success);
+        return this;
+    }
+
+    public Result code(Integer code) {
+        this.setCode(code);
+        return this;
+    }
+
+    public Result message(String message) {
+        this.setMessage(message);
+        return this;
+    }
+
+    public Result data(Map<String, Object> data) {
+        this.setData(data);
+        return this;
+    }
+}
---
+++ common/common-utils/src/main/java/com/company/common/ResultCode.java
@@ -0,0 +1,19 @@
+package com.company.common;
+
+/**
+ * @author: yaochunguang
+ * @date: 2021-03-03 19:45
+ * @description: 返回码
+ **/
+public interface ResultCode {
+
+    /**
+     * 成功
+     */
+    Integer SUCCESS = 20000;
+
+    /**
+     * 失败
+     */
+    Integer ERROR = 20001;
+}
